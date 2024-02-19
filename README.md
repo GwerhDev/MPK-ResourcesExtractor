416a654bafc9223b95a95a128e38a2b5b42e1803
GwerhDev <gwerh.dev@gmail.com>
2024-02-19 15:46:15 -0300

    functions and interface

---
+++ README.md
@@ -0,0 +1,5 @@
+# ExtractorResourcesMPK
+
+This project is based on https://github.com/mindphluxnet/MPKExtractor
+
+Requires Python 3.8.x
---
+++ main.py
@@ -0,0 +1,44 @@
+import PySimpleGUI as sg
+import glob
+import os
+from functions import *
+
+def select_file():
+    layout = [
+        [sg.Text("Select MPKINFO file:")],
+        [sg.Input(key="-FILE-"), sg.FileBrowse()],
+        [sg.Button("Extract"), sg.Button("Cancel")]
+    ]
+
+    window = sg.Window("MPK Extractor", layout)
+
+    while True:
+        event, values = window.read()
+        if event == sg.WINDOW_CLOSED or event == "Cancel":
+            window.close()
+            return None
+        elif event == "Extract":
+            return values["-FILE-"]
+
+if __name__ == "__main__":
+    mpkinfo_file = select_file()
+    if mpkinfo_file:
+        basename = ""
+        resource_files = []
+        resource_info = mpkinfo_file
+
+        if(mpkinfo_file.lower().endswith(".mpkinfo")):
+            # fix mismatching file names for Resource*.mpk
+            if(mpkinfo_file.lower().startswith("resource")):
+                basename = "Resources"
+            else:
+                basename = mpkinfo_file.split(".")[0]
+
+            mpk_files = glob.glob(basename + "*.mpk")
+
+            resource_files.append(basename + ".mpk")
+
+            unpack_archive(mpkinfo_file, resource_files)  # Extract directly after file selection
+
+        else:
+            sg.popup_error("Please select a valid MPKINFO file.")
