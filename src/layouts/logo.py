262439bb05c67b1939b26e8a80bc1183b7384fb7
GwerhDev <gwerh.dev@gmail.com>
2024-02-21 12:27:39 -0300

    documenting

--- src/layouts/logo.py
+++ src/layouts/logo.py
@@ -1,6 +1,6 @@
 import PySimpleGUI as sg
 
-image_path = "src/assets/logo.png"
+logo_path = "src/assets/logo.png"
 layout_logo = [
-  [sg.Image(filename=image_path, size=(380, 380), background_color='#15171E')],
+  [sg.Image(source=logo_path, size=(380, 380), background_color='#15171E')],
 ]
